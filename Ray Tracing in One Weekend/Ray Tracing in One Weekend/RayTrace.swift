f96906bc09101e4943331ec49ef2eaf98708e3d8
Dave <snpukwana@gmail.com>
2018-03-17 15:34:13 -0400

    Chapter 7

--- Ray Tracing in One Weekend/Ray Tracing in One Weekend/RayTrace.swift
+++ Ray Tracing in One Weekend/Ray Tracing in One Weekend/RayTrace.swift
@@ -19,10 +19,19 @@ extension Pixel {
     }
 }
 
+func randomInUnitSphere() -> Vec3 {
+    var p: Vec3
+    repeat {
+        p = 2.0 * Vec3(Float(drand48()), Float(drand48()), Float(drand48())) - Vec3(1, 1, 1)
+    } while p.squaredLength >= 1.0
+    return p
+}
+
 func color(_ r: Ray, world: Hitable) -> Vec3 {
     var rec: HitRecord = HitRecord(t: 0.0, p: Vec3(0, 0, 0), normal: Vec3(0, 0, 0)) // dummies because needs to be initialized
-    if world.hit(r: r, tMin: 0.0, tMax: Float.greatestFiniteMagnitude, rec: &rec)  {
-        return 0.5 * Vec3(rec.normal.x + 1, rec.normal.y + 1, rec.normal.z + 1)
+    if world.hit(r: r, tMin: 0.001, tMax: Float.greatestFiniteMagnitude, rec: &rec)  {
+        let target: Vec3 = rec.p + rec.normal + randomInUnitSphere()
+        return 0.5 * color(Ray(rec.p, target - rec.p), world: world)
     }
     let unitDirection: Vec3 = r.direction.unitVector
     let t: Float = 0.5 * (unitDirection.y + 1.0)
@@ -49,6 +58,7 @@ func rayTrace(width: Int, height: Int) -> [Pixel] {
                 col += color(r, world: world)
             }
             col /= Float(numSamples)
+            col = Vec3(col.x.squareRoot(), col.y.squareRoot(), col.z.squareRoot()) // raise to gamma 1/2
             let ir: UInt8 = UInt8(255.99 * col.r)
             let ig: UInt8 = UInt8(255.99 * col.g)
             let ib: UInt8 = UInt8(255.99 * col.b)
